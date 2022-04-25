631e42f82423f9e4313ef50e450776cacdc9985b
remibareille <remi.bareille.edu@groupe-gema.com>
2022-04-25 15:36:35 +0200

    deleted file

--- airquality.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv('/Users/remibareille/Downloads/indice_ATMO.csv')
-data.head()
-
-data_rhône = data[(data['X'] > 4.62477) & (data['X'] < 5.221920) & (data['Y'] > 43.174200) & (data['Y']<45.764043)]
-data_rhône
-
